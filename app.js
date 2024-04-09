bfe4811171930b2fb2ba2200b427af0365442bc0
ComfyJace <jason_erwin_dimalanta@dlsu.edu.ph>
2024-04-09 18:13:14 +0800

    feat: db connection

--- app.js
+++ app.js
@@ -1,45 +1,47 @@
+// Import dependencies
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
 require('dotenv').config(); // Load environment variables from .env file
 
+// Define app router and use cors
 const app = express();
+app.use(cors())
 
-// MySQL Connection Configuration
-const connection = mysql.createConnection({
+// Establish db connection
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  port:  process.env.DB_PORT,
+  user:  process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
 });
 
+
+app.get('/', (req, res)=>{
+  const sql = "SELECT * FROM global_records"
+  db.query(sql, (err, data) =>{
+    if(err) return res.json(err);
+    return res.json(data);
+  })
+  return res.json("From backend side")
+});
+
+
 // Connect to MySQL
-connection.connect((err) => {
+db.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL: ', err);
+    console.error('Error connecting to MySQL: ', err.stack);
     return;
   }
   console.log('Connected to MySQL');
 });
 
-// Define API endpoint to fetch data
-app.get('/api/data', (req, res) => {
-  // Execute SELECT query to retrieve data from table
-  const query = 'SELECT * FROM your_table_name';
-  connection.query(query, (err, results) => {
-    if (err) {
-      console.error('Error executing MySQL query: ', err);
-      res.status(500).json({ error: 'Internal server error' });
-      return;
-    }
-    // Log the retrieved data to the console
-    console.log('Retrieved data:', results);
-    // Send data as JSON response
-    res.json(results);
-  });
-});
 
 // Start the server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
